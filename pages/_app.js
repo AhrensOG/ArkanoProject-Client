4f0d80a609be95278dd8eb7bbe04d19360f6fb15
AhrensOG <ahrensgabriel09@gmail.com>
2022-12-15 10:33:44 -0300

    Initial commit | Add TawilwindCSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.css'
 
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  variable: '--font-roboto',
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${roboto.variable} font-roboto`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
