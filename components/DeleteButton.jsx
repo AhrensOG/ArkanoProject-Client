1c3ab38dc05ee5d127b1f33f964be16ba48a02ce
Ivixx09 <estudio.xx23@gmail.com>
2022-12-15 19:59:25 -0300

    Trash button added

---
+++ components/DeleteButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {TfiTrash} from "react-icons/tfi"
+
+const DeleteButton = () => {
+  return (
+    <div>
+      <button>
+        <TfiTrash/>
+      </button>
+    </div>
+    )
+}
+
+export default DeleteButton
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['var(--font-roboto)', ...fontFamily.sans]
+        // roboto: ['var(--font-roboto)', ...fontFamily.sans],
+        custom: "'Lora', serif"
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
