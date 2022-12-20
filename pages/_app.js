d9734c8e1398ece68f25549d97a8a6163b8816a3
Ivixx09 <estudio.xx23@gmail.com>
2022-12-20 04:46:56 -0300

    NavBar modified for dinamic situation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 
 import { Roboto } from '@next/font/google'
+// import NavBar from '../components/Navbar/NavBar'
+import SideBar from '../components/Navbar/SideBar'
+import { useState } from 'react'
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -11,6 +14,8 @@ const roboto = Roboto({
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${roboto.variable} font-roboto`}>
+      {/* <NavBar nav={nav} setNav={setNav} state={"welcome"}/> */}
+      <SideBar nav={nav} setNav={setNav}/>
       <Component {...pageProps} />
     </main>
   )
