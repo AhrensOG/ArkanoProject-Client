e6c67883aaa40a08743e5459caebe1546d373421
Ivixx09 <estudio.xx23@gmail.com>
2022-12-26 06:32:09 -0300

    fix conflicts

--- pages/endMerch.js
+++ pages/endMerch.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import EndingPage from '../components/EndingPage/EndingPage';
 import NavBar from '../components/Navbar/NavBar';
 import { useState } from 'react';
+import SideBar from '../components/Navbar/SideBar';
 
 export default function EndMerch() {
 
@@ -17,7 +18,7 @@ return (
         </Head>
         <div>
             <NavBar nav={nav} setNav={setNav} />
-            <SideBar nav={nav} setNav={setNav}/>
+            <SideBarar nav={nav} setNav={setNav}/>
 
             <EndingPage/>
         </div>
