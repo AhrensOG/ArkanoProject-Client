31b395e810c23aed01a114fb0df9fb2fd1dfb050
Ivixx09 <estudio.xx23@gmail.com>
2022-12-26 07:01:43 -0300

    Deleted line navBar

--- components/EndingPage/EndingPage.jsx
+++ components/EndingPage/EndingPage.jsx
@@ -4,10 +4,8 @@ import NavBar from "../Navbar/NavBar"
 import DirectionButtonsControllers from "../buttons/DirectionButtonsControllers"
 
 const EndingPage = () => {
-    const [nav, setNav] = useState(false)
     return (
         <>
-            <NavBar nav={nav} setNav={setNav}/>
             <div className='bg-gradient-to-b from-[#00FFFF] via-[#E73D93] to-[#9400B3]'>
                 <div className='bg-white/70 pt-6 flex flex-col gap-12'>
                     <div className='flex flex-row justify-center px-6'>
