52e99ce32ff727ff95692472bea8ff7187f469fa
mruggeroni <matiasruggeroni@gmail.com>
2022-12-27 17:51:51 -0300

    Created ImageBar

---
+++ components/controlsDesignToolBar/imageBar/ImageSelect.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SelectBar from '../SelectBar/SelectBar';
+
+
+const ImageSelect = () => {
+    return (
+        <div>
+            <div className="bg-[#313131] flex justify-center items-center w-full h-16" >
+                <input className=" text-white" type="file" ></input>
+            </div>
+            <SelectBar array={["Superior", "Inferior", "Cuello", "Posterior"]} handleSelect={() => 1} />
+        </div>
+    );
+};
+
+export default ImageSelect;
