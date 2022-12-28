567befa1ce6131cfb79d83adb03131cb3bbd8179
AhrensOG <ahrensgabriel09@gmail.com>
2022-12-28 13:00:27 -0300

    Change some styles

--- components/PartClothes/ColorShirt.jsx
+++ components/PartClothes/ColorShirt.jsx
@@ -1,17 +1,17 @@
 
 const ColorShirt = () => {
 
-    const data = ["manga", "cuello", "SUPERIOR", "INFERIOR", "atrás", "Otros" ]
+    const data = ["Mangas", "Cuello", "Atrás", "Frente" ]
 
     return (
-        <div className="flex bg-[#313131] w-full h-30% justify-between">
+        <div className="flex bg-[#313131] w-full justify-between">
         {data.map((e) => {
             return (
-                <div key={e} value={e} className="flex justify-center  py-3 hover:bg-[#FFFFFF] focus:[#FFFFFF] h-12 w-full">
+                <div key={e} value={e} className="flex justify-center py-1 hover:bg-white focus:bg-[#FFFFFF] w-full">
                     <button
-                    className=""
+                    className="text-white hover:text-black"
                     >
-                    {e.toUpperCase()}
+                    {e}
                     </button>
                 </div>
             );
