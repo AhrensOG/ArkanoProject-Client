f87885d0917005203fecfe10dc7043d0d6efb63f
AhrensOG <ahrensgabriel09@gmail.com>
2022-12-28 12:59:02 -0300

    Changes some styles in the component

--- components/EndingPage/EndingPage.jsx
+++ components/EndingPage/EndingPage.jsx
@@ -1,24 +1,20 @@
-import ConfirmationOptions from "../buttons/ConfirmationOptions"
-import { useState } from "react"
-import NavBar from "../Navbar/NavBar"
-import DirectionButtonsControllers from "../buttons/DirectionButtonsControllers"
+import ConfirmationOptions from "../buttons/ConfirmationOptions";
+import DirectionButtonsControllers from "../buttons/DirectionButtonsControllers";
 
 const EndingPage = () => {
-    return (
-        <>
-            <div className='bg-gradient-to-b from-[#00FFFF] via-[#E73D93] to-[#9400B3]'>
-                <div className='bg-white/70 pt-6 flex flex-col gap-12'>
-                    <div className='flex flex-row justify-center px-6'>
-                        <img src="/sun.svg" alt="" />
-                    </div>
-                    <div className='flex flex-col gap-6 text-center tracking-wide'>
-                        <ConfirmationOptions/>
-                        <DirectionButtonsControllers goHome={true}/>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="h-full flex flex-col justify-center gap-20 bg-gradient-to-b from-[#B2FFFF] via-[#F1C3E0] to-[#d2bfd6] py-12">
+        <div className="flex flex-row justify-center px-6">
+          <img src="/sun.svg" alt="" />
+        </div>
+        <div className="flex flex-col gap-6 text-center tracking-wide">
+          <ConfirmationOptions />
+        </div>
+      </div>
+      <DirectionButtonsControllers goHome={true} />
+    </>
+  );
+};
 
-export default EndingPage
+export default EndingPage;
