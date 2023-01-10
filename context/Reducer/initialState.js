2a6fe2a60fe8e40ea6937a58cc6ae0f40560b514
mruggeroni <matiasruggeroni@gmail.com>
2023-01-10 15:05:12 -0300

    Initialization of the linked list within the initial state

--- context/Reducer/initialState.js
+++ context/Reducer/initialState.js
@@ -1,25 +1,38 @@
-const state = {
-    clothes: {
-        class: 'Remera',
-        color: {
-            Frontal: '',
-            Posterior: '',
-            Mangas: '',
-            Cuello: '',
-        },
-        text: [{
-            zona: 'frontal', // frontal o posterior
-            HTML: '<p>Nueva REMERA</p>',
-        }],
-        image: [{
-            zona: 'posterior', // frontal, posterior, mangaDerecha o mangaIzquierda
-            url: 'http://URL.com',
-        }],
+import LinkedList from '../../linkedList/linkedList';
+
+
+// Estructura de datos de la prenda
+const clothes = {
+    class: 'Remera',
+    color: {
+        Frontal: '',
+        Posterior: '',
+        Mangas: '',
+        Cuello: '',
     },
+    text: [{
+        zona: 'frontal', // frontal o posterior
+        HTML: '<p>Nueva REMERA</p>',
+    }],
+    image: [{
+        zona: 'posterior', // frontal, posterior, mangaDerecha o mangaIzquierda
+        url: 'http://URL.com',
+    }],
 };
 
-export const remeraColorZones = Object.keys(state.clothes.color);
+// Inicialización de la LinkedList
+const list = new LinkedList();
+list.insert(clothes);
 
+// Preinicialización del estado inicial
+const state = {
+    linkedList: list,
+    currentNode: list.point,
+    clothes,
+};
+
+// Armado de la parte dinámica del estado
+export const remeraColorZones = Object.keys(state.clothes.color);
 export const addtionalClothesColorZone = {
     Remera: {},
     Buzo: {
@@ -32,9 +45,11 @@ export const addtionalClothesColorZone = {
     },
 };
 
+// Inicialización del estado inicial actualizando la preinicialización con la parte dinámica
 state.clothes.color = {
     ...state.clothes.color,
     ...addtionalClothesColorZone[state.clothes.class],
 };
+state.linkedList.insert(state.clothes, 1);
 
 export default state;
