d47436882692daadc750e500702d82703580b64e
mruggeroni <matiasruggeroni@gmail.com>
2023-01-21 17:50:02 -0300

    Installed and configured SVGR and SVGO plugin for import SVG file with id property

--- next.config.js
+++ next.config.js
@@ -1,13 +1,41 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // https://react-svgr.com/docs/next/
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-
-    return config
+      use: [ // Se podría usar solo "@svgr/webpack" en esta linea para concluir la configuración de SVGR con Webpack
+      {
+        loader: "@svgr/webpack",
+        options: {
+          // para poder dar referencias a los SVG y usar useRef()
+          ref: true,
+          // ^^^^^^
+          // Configuración del plugin SVGO para traer los ID en los SVG:
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      cleanupIDs: {
+                        remove: false,
+                        // minify: false,
+                        // prefix: 'xxx',
+                        force: true,
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+        // ^^^^^^
+      ],
+    });
+    return config;
   },
-}
+};
